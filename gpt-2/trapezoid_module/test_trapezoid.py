5e081d3a33cbf3d3d451bca5ca6e931f1ca565c1
tgx <tangexing.tgx@alibaba-inc.com>
2025-06-04 11:23:20 +0800

    mac commit

--- gpt-2/trapezoid_module/test_trapezoid.py
+++ gpt-2/trapezoid_module/test_trapezoid.py
@@ -89,7 +89,7 @@ def test_hbm_matrices():
 def test_hbm_small_matrices():
     # 创建稀疏矩阵
     np.random.seed(42)
-    M, K, N = 1, 128, 128
+    M, K, N = 1, 512, 512
 
     # 随机生成稀疏矩阵
 
@@ -98,10 +98,10 @@ def test_hbm_small_matrices():
     print(B)
     #M, K, N = 1, 4, 3
     expected_C = naive_matmul(A, B)
-    pipeline = TrapezoidPipeline(M=M, K=K, N=N, PE_num=32)
+    pipeline = TrapezoidPipeline(M=M, K=K, N=N, PE_num=128)
 
-    hbm_data_lists = store_csr_in_simple_blocks(csr_matrix(B.T), 32)
-    print(hbm_data_lists)
+    hbm_data_lists = store_csr_in_simple_blocks(csr_matrix(B.T), 256)
+    #print(hbm_data_lists)
     print("\n运行流水线...")
     start_time = time.time()
     result = pipeline.run_pipeline_hbm_with_bf16([A], hbm_data_lists)
@@ -130,7 +130,7 @@ def test_hbm_small_matrices():
 def test_hbm_multi_small_matrices():
     # 创建稀疏矩阵
     np.random.seed(42)
-    M, K, N = 1, 128, 128
+    M, K, N = 1, 512, 512
 
     # 随机生成稀疏矩阵
 
@@ -140,16 +140,16 @@ def test_hbm_multi_small_matrices():
     #M, K, N = 1, 4, 3
     expected_C = naive_matmul(A, B)
 
-    num_trapezoids = 4
+    num_trapezoids = 32
     trapezoid_list = []
 
     for i in range(num_trapezoids):
-        trap = TrapezoidPipeline(M, K, N, 32)
+        trap = TrapezoidPipeline(M, K, N, 128)
         trapezoid_list.append(trap)
 
     main_trap = trapezoid_list[0]
 
-    hbm_data_lists = store_csr_in_simple_blocks(csr_matrix(B.T), 32)
+    hbm_data_lists = store_csr_in_simple_blocks(csr_matrix(B.T), 256)
     #print(hbm_data_lists)
     print("\n运行流水线...")
     start_time = time.time()
